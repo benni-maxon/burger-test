68e0dc235391ee676ec8a6fc442bea94b9cdad3b
benni maxon <benni.maxon@gmail.com>
2023-02-09 14:52:14 -0800

    changed colors in theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 export const theme = {
-  primaryDark: "#0D0C1D",
-  primaryLight: "#EFFFFA",
-  primaryHover: "#343078",
+  primaryDark: "rgb(57, 57, 57)",
+  primaryLight: "rgb(108, 108, 203)",
+  primaryHover: "rgba(57, 57, 57, .69)",
   mobile: "576px",
 };
